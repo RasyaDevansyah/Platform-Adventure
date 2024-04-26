436961b837d6feb666f9aadc313c02cb540300a6
Rasya Devansyah <1.moment.i.got.hacked@gmail.com>
2024-04-26 14:55:51 +0700

    map

--- Scripts/PickableItem.gd
+++ Scripts/PickableItem.gd
@@ -9,7 +9,6 @@ func _ready():
 	await get_tree().create_timer(rng.randf_range(0,0.3)).timeout
 	animated_sprite_2d.play("default")
 
-#dwaddwadwadwdwawd
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("player"):
 		animated_sprite_2d.play("pickedUp")
